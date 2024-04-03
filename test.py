c2598a767359a88a0cbaf85b3ac56a012fb1aea3
lmonsalve22 <lmonsalve22@gmail.com>
2024-04-03 19:03:22 -0300

    no entiendo el problema

--- test.py
+++ test.py
@@ -120,5 +120,6 @@ if __name__ == '__main__':
     personal["Nombres2"] = personal["Nombres2"].apply(transformar_string) 
     personal["NOMBRECOMPLETO2"] = personal["NOMBRECOMPLETO"].apply(transformar_string) 
     for i in personal["organismo_nombre"].unique()[:5]:
+        print(i)
         organismo = personal[personal["organismo_nombre"] == i]
         organismo.to_excel(f"organismo/{i}.xlsx", index=False)
