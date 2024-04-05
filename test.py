e3bff9718f621091cf758909751f8c1ba91a425d
lmonsalve22 <lmonsalve22@gmail.com>
2024-04-05 17:31:59 -0300

    sin limpieza

--- test.py
+++ test.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
     #listaDF = [readCSV(name) for name in getFiles("shared")]
     
     #personal = pd.concat(listaDF)
-    #df1 = pd.read_csv("shared/TA_PersonalPlanta.csv", low_memory=False,sep=";",encoding="latin",usecols=PersonalPlantaDICT)
+    df1 = pd.read_csv("shared/TA_PersonalPlanta.csv", low_memory=False,sep=";",encoding="latin",usecols=PersonalPlantaDICT)
     print(2)
     df2 = pd.read_csv("shared/TA_PersonalContrata.csv", sep=";",encoding="latin",usecols=PersonalContrataDICT)
     print(3)
@@ -123,14 +123,14 @@ if __name__ == '__main__':
     print(4)
     df4 = pd.read_csv("shared/TA_PersonalContratohonorarios.csv", sep=";",encoding="latin",usecols=PersonalContratohonorariosDICT)
     df4 = df4.rename(columns={'remuneracionbruta': 'remuneracionbruta_mensual'})
-    personal = pd.concat([df2,df3,df4])
+    personal = pd.concat([df1,df2,df3,df4])
     print(5)
-    personal["remuneracionbruta_mensual"] = personal["remuneracionbruta_mensual"].apply(getFloat)
-    personal["remuliquida_mensual"] = personal["remuliquida_mensual"].apply(getFloat)
-    personal["Nombres2"] = personal["Nombres"].apply(eliminar_espacios_adicionales)
-    personal["NOMBRECOMPLETO"] = personal.apply(getFullName,axis=1)
-    personal["Nombres2"] = personal["Nombres2"].apply(transformar_string) 
-    personal["NOMBRECOMPLETO2"] = personal["NOMBRECOMPLETO"].apply(transformar_string) 
+    #personal["remuneracionbruta_mensual"] = personal["remuneracionbruta_mensual"].apply(getFloat)
+    #personal["remuliquida_mensual"] = personal["remuliquida_mensual"].apply(getFloat)
+    #personal["Nombres2"] = personal["Nombres"].apply(eliminar_espacios_adicionales)
+    #personal["NOMBRECOMPLETO"] = personal.apply(getFullName,axis=1)
+    #personal["Nombres2"] = personal["Nombres2"].apply(transformar_string) 
+    #personal["NOMBRECOMPLETO2"] = personal["NOMBRECOMPLETO"].apply(transformar_string) 
     print(6)
     for i in personal["organismo_nombre"].unique()[:15]:
         organismo = personal[personal["organismo_nombre"] == i]
