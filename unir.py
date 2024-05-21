f2c684e21800422587d9198813c1f2c81ae6ad85
lmonsalve22 <lmonsalve22@gmail.com>
2024-05-21 02:37:20 -0400

    unir

--- unir.py
+++ unir.py
@@ -1,5 +1,28 @@
 import pandas as pd
+import os
 
 
 if __name__ == '__main__':
     print("Todo bien por aki")
+    df = pd.read_excel("organismos.xlsx")
+    
+    for i in df["organismo_nombr"]:
+        print(i)
+        df1 = pd.DataFrame()
+        df2 = pd.DataFrame()
+        df3 = pd.DataFrame()
+        df4 = pd.DataFrame()
+        if os.path.exists(f"shared_1/{i}"):
+            df1 = pd.read_excel(f"shared_1/{i}.xlsx")
+        if os.path.exists(f"shared_2/{i}"):
+            df2 = pd.read_excel(f"shared_2/{i}.xlsx")
+        if os.path.exists(f"shared_3/{i}"):
+            df1 = pd.read_excel(f"shared_3/{i}.xlsx")
+        if os.path.exists(f"shared_4/{i}"):
+            df1 = pd.read_excel(f"shared_4/{i}.xlsx")
+        concat = pd.concat([df1,df2,df3,df4])
+        concat.to_excel(f"unir/{i}.xlsx", index=False)
+    print("Termino de guardar en excel")
+
+        
+        
