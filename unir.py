8f8a40c9bbccb1bfadb37656cd053de1c1f2ce34
lmonsalve22 <lmonsalve22@gmail.com>
2024-05-22 15:47:24 -0400

    mostrar archivos

--- unir.py
+++ unir.py
@@ -5,13 +5,18 @@ import os
 if __name__ == '__main__':
     print("Todo bien por aki")
     df = pd.read_excel("organismos.xlsx")
-    
+    print(os.listdir("shared_1"))
+    print(os.listdir("shared_2"))
+    print(os.listdir("shared_3"))
+    print(os.listdir("shared_4"))
+
     for i in df["organismo_nombre"]:
         print(i)
         df1 = pd.DataFrame()
         df2 = pd.DataFrame()
         df3 = pd.DataFrame()
         df4 = pd.DataFrame()
+        
         if os.path.exists(f"shared_1/{i}"):
             df1 = pd.read_excel(f"shared_1/{i}.xlsx")
         if os.path.exists(f"shared_2/{i}"):
