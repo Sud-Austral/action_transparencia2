5511cbd6772ee02e15c3567eb0bb68ddda65e9d5
lmonsalve22 <lmonsalve22@gmail.com>
2024-05-22 18:55:36 -0400

    Update unir.py

--- unir.py
+++ unir.py
@@ -5,11 +5,11 @@ import os
 if __name__ == '__main__':
     print("Todo bien por aki")
     df = pd.read_excel("organismos.xlsx")
-    print(os.listdir("."))
-    print(os.listdir("shared_1"))
-    print(os.listdir("shared_2"))
-    print(os.listdir("shared_3"))
-    print(os.listdir("shared_4"))
+    #print(os.listdir("."))
+    #print(os.listdir("shared_1"))
+    #print(os.listdir("shared_2"))
+    #print(os.listdir("shared_3"))
+    #print(os.listdir("shared_4"))
 
     for i in df["organismo_nombre"]:
         print(i)
@@ -18,13 +18,13 @@ if __name__ == '__main__':
         df3 = pd.DataFrame()
         df4 = pd.DataFrame()
         
-        if os.path.exists(f"shared_1/{i}"):
+        if os.path.exists(f"shared_1/{i}.xlsx"):
             df1 = pd.read_excel(f"shared_1/{i}.xlsx")
-        if os.path.exists(f"shared_2/{i}"):
+        if os.path.exists(f"shared_2/{i}.xlsx"):
             df2 = pd.read_excel(f"shared_2/{i}.xlsx")
-        if os.path.exists(f"shared_3/{i}"):
+        if os.path.exists(f"shared_3/{i}.xlsx"):
             df1 = pd.read_excel(f"shared_3/{i}.xlsx")
-        if os.path.exists(f"shared_4/{i}"):
+        if os.path.exists(f"shared_4/{i}.xlsx"):
             df1 = pd.read_excel(f"shared_4/{i}.xlsx")
         concat = pd.concat([df1,df2,df3,df4])
         concat.to_excel(f"unir/{i}.xlsx", index=False)
