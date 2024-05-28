784feaf9b3b94495add5520599b8ae353a299bd4
lmonsalve22 <lmonsalve22@gmail.com>
2024-05-28 00:38:23 -0400

    python

--- build4.py
+++ build4.py
@@ -81,7 +81,7 @@ output_file = 'TA_PersonalContratohonorarios.csv'
 chunk_size = 1048576  # 1 MB
 
 def download_file(url, output_file, chunk_size):
-    with requests.get(url, stream=True) as response:
+    with requests.get(url, stream=True,, verify=False) as response:
         response.raise_for_status()
         with open(output_file, 'wb') as file:
             for chunk in response.iter_content(chunk_size=chunk_size):
